04cf372808027b4b4d36272cef7e20ebfe456cc0
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2018-08-13 10:40:46 -0300

    service work in POST or GET

--- index.js
+++ index.js
@@ -8,6 +8,71 @@
 
   let app = express();
 
+  /**
+   * 
+   * dimensions = {width : 123, height : 123}
+   * 
+   */
+  let resize = (image, dimensions = {}) => {
+    return new Promise((resolve, reject) => {
+      if (!image) {
+        reject('Required image to resize');
+        return;
+      }
+
+      if (!dimensions.width && !dimensions.height) {
+        reject('Required width OR height to resize image');
+        return;
+      }
+
+      const optionsFeatures = {
+        url: image, 
+        dest: './tmp'
+      }
+
+      console.debug(`optionsFeatures => ${util.inspect(optionsFeatures)}`);
+
+      download.image(optionsFeatures).then(({ filename, image }) => {
+        im.identify(filename, function (err, features) {
+          if (err) {
+            // res.status(500).send(`quebrou véi... ${util.inspect(err)}`);
+            reject(`error identify ==>> ${util.inspect(err)}`);
+          }
+
+          console.log(util.inspect(features));
+
+          let destPath = `./tmp/${new Date().valueOf()}.${features.format}`;
+          const optionsResize = {
+            srcPath: filename,
+            dstPath: destPath
+          }
+
+          if (dimensions.width) optionsResize.width = dimensions.width;
+          if (dimensions.height) optionsResize.height = dimensions.height;
+
+          // console.log("options resize", util.inspect(optionsResize));
+
+          im.resize(optionsResize, (err, stdout, stderr) => {
+            if (err) { 
+              // console.error("error resize ", util.inspect(err));
+              reject(`error resize ${util.inspect(err)}`);
+            }
+
+            im.identify(destPath, function (err, featuresResized) {
+              console.log(`Original size (WxH):${features.width}x${features.height} - resized size (WxH):${featuresResized.width}x${featuresResized.height}`);
+            });
+
+            // res.sendFile(path.join(process.cwd(), destPath));
+            resolve(destPath);
+          });
+        });
+      }).catch((err) => {
+        // res.status(500).send(`quebrou véi... ${util.inspect(err)}`);
+        reject(`quebrou véi... ${util.inspect(err)}`);
+      });
+    });
+  }
+
   app.use(express.json());
 
   app.use((err, req, res, next) => {
@@ -28,47 +93,28 @@
     console.log('POST /resize');
     console.log('   body ' + util.inspect(req.body));
 
-    if (req.body && req.body.remote_image) {
-      // console.log('remote_image ' + req.body.remote_image);
 
-      const optionsFeatures = {
-        url: req.body.remote_image,
-        dest: './tmp'
-      }
+    let dimensions = {};
+    if (req.body.width) { dimensions.width = req.body.width }
+    if (req.body.height) { dimensions.height = req.body.height }
 
-      download.image(optionsFeatures).then(({ filename, image }) => {
-        im.identify(filename, function (err, features) {
-          if (err) res.status(500).send(`quebrou véi... ${util.inspect(err)}`);
+    resize(req.body.remote_image, dimensions).then(pathImageResized => {
+      res.sendFile(path.join(process.cwd(), pathImageResized));
+    }).catch(err => res.status(500).send(err));
+  });
 
-          // console.log(util.inspect(features));
+  app.get('/resize', (req, res) => {
+    console.log('GET /resize');
+    console.log('   query ' + util.inspect(req.query));
 
-          let destPath = `./tmp/${new Date().valueOf()}.${features.format}`;
-          const optionsResize = {
-            srcPath: filename,
-            dstPath: destPath
-          }
 
-          if (req.body.width) optionsResize.width = req.body.width;
-          if (req.body.height) optionsResize.height = req.body.height;
+    let dimensions = {};
+    if (req.query.width) { dimensions.width = req.query.width }
+    if (req.query.height) { dimensions.height = req.query.height }
 
-          // console.log("options resize", util.inspect(optionsResize));
-
-          im.resize(optionsResize, (err, stdout, stderr) => {
-            if (err) console.error("error resize ", util.inspect(err)); // res.status(500).send(`quebrou véi... ${util.inspect(err)}`);
-
-            im.identify(destPath, function (err, featuresResized) {
-              console.log(`Original size (WxH):${features.width}x${features.height} - resized size (WxH):${featuresResized.width}x${featuresResized.height}`);
-            });
-
-            res.sendFile(path.join(process.cwd(), destPath));
-          });
-        });
-      }).catch((err) => {
-        res.status(500).send(`quebrou véi... ${util.inspect(err)}`);
-      });
-    } else {
-      res.json();
-    }
+    resize(req.query.remote_image, dimensions).then(pathImageResized => {
+      res.sendFile(path.join(process.cwd(), pathImageResized));
+    }).catch(err => res.status(500).send(err));
   });
 
   app.listen(5555, () => {
